b56041b779cdbfd569ee790bdc91a73a99acf509
Nils Svenungsson <Nilssvenungsson@gmail.com>
2024-12-10 10:11:02 +0100

    nytt

--- MyWorld.cs
+++ MyWorld.cs
@@ -9,10 +9,17 @@ namespace EasyStart
 {
     internal class MyWorld : World
     {
-        public MyWorld() : base(600, 800)
+        public MyWorld() : base(1920, 1080)
         {
             // Tile background with the file "bluerock" in the Content folder.
             BackgroundTileName = "bluerock";
+            Add(new Player(), "mario", 250, 500);
+            for (int x = 20; x < 1920; x += 40)
+            {
+                Add(new Ground(), "Brick", x, 1012);
+                Add(new Ground(), "Brick", x, 972);
+                Add(new Ground(), "Brick", x, 932);
+            }
         }  
     }
 }
---
+++ Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using EasyMonoGame;
+using Microsoft.Xna.Framework.Input;
+
+namespace EasyStart
+{
+    internal class Player : Actor
+    {
+        private float gravity = -0.5f;
+        private float jumpSpeed = 10f;
+        private float ySpeed = 0;
+
+        public override void Act()
+        {
+            if (!IsTouching(typeof(Ground)))
+            {
+                // i fritt fall
+                ySpeed += gravity;
+                Y = Y - ySpeed;
+            }
+
+            else
+            {
+                // på marken
+                Y += ySpeed; //TODO kompensera mindre, bara så att man kommer upp till markytan men ej över
+                ySpeed = 0;
+               
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W) && IsTouching(typeof(Ground)))
+            {
+                ySpeed = jumpSpeed;
+                Y -= jumpSpeed;
+            }
+        }
+    }
+}
